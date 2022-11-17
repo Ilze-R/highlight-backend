d7d08fe49628177cce8414b85eb2b780c8b0a292
Art <d.art.shishkin@gmail.com>
2022-11-17 21:56:47 +0200

    Starting MySQL in docker compose

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+USE highlight ;
+INSERT INTO group_collection (create_time, description, group_name, open, strict, size) VALUES (current_timestamp() ,'this is test this is test', 'We do', true, false, 10);
+
+
+
+USE highlight ;
+INSERT INTO group_collection (create_time, description, group_name, open, strict, size) VALUES (current_timestamp() ,'all about cats', 'muris', true, true, 10);
